c354031da5a547eec2da090ab25170c56e1b0c5c
echung <elbo1216@gmail.com>
2011-01-31 23:38:07 -0500

    Added a bunch of stuff

--- app/controllers/admin/event_controller.rb
+++ app/controllers/admin/event_controller.rb
@@ -3,29 +3,34 @@ module Admin
 	layout "admin_layout"
 
 	def index
-      @events = Event.find_by_sql("select * from events e join upcoming_events ue on ue.event_id = e.id order by e.id desc limit 20")
+      @upcoming_asian_events = UpcomingEvent.find_by_sql("select * from upcoming_events ue where event_type = 'a' order by event_order") 
+      @upcoming_mixed_events = UpcomingEvent.find_by_sql("select * from upcoming_events where event_type = 'm' order by event_order") 
+      if @upcoming_asian_events.size < UpcomingEvent::MAX_EVENTS || @upcoming_mixed_events.size < UpcomingEvent::MAX_EVENTS
+        UpcomingEvent.synchronize_upcoming_events
+      end
+      @events = Event.find_by_sql("select * from events e order by e.id desc limit 20")
 	end
 
     def create
 	  @method = 'Create'
+      @event = Event.new
+      @flyer = Flyer.new
       if request.post?
-        flyer = Flyer.new
-        flyer.upload_flyer(params['flyer'])
-        flyer.save
+        @flyer.upload_flyer(params['flyer'])
+        @flyer.save
 
-        event = Event.new
-        event.event_name = params['event_name']
-        event.event_address = params['event_address']
-        event.venue = params['venue']
-        event.flyer_id = flyer.id
-        event.event_start_time = params['event_start_time']
-        event.event_notes1_label = params['event_notes1_label']
-        event.event_notes1 = params['event_notes1']
-        event.event_notes1_label = params['event_notes2_label']
-        event.event_notes1 = params['event_notes2']
-        event.event_notes1_label = params['event_notes3_label']
-        event.event_notes1 = params['event_notes3']
-        event.save!
+        @event.event_name = params['event_name']
+        @event.event_address = params['event_address']
+        @event.venue = params['venue']
+        @event.flyer_id = flyer.id
+        @event.event_start_time = params['event_start_time']
+        @event.event_notes1_label = params['event_notes1_label']
+        @event.event_notes1 = params['event_notes1']
+        @event.event_notes1_label = params['event_notes2_label']
+        @event.event_notes1 = params['event_notes2']
+        @event.event_notes1_label = params['event_notes3_label']
+        @event.event_notes1 = params['event_notes3']
+        @event.save!
 
         flash[:notice] = "Event Saved"
         redirect_to :action => 'index'
@@ -35,32 +40,39 @@ module Admin
 
     def update
 	  @method = 'Update'
-      @event = Event.find_by_sql("select * from events e left join upcoming_events ue on ue.event_id = e.id where e.id = #{params['id']}").first
+      @event = Event.find_by_sql("select * from events e where e.id = #{params['id']}").first
+      @flyer = @event.flyer || Flyer.new
       if request.post?
         if params['flyer'].blank?
-          flyer = Flyer.new
-          flyer.upload_flyer(params['flyer'])
-          flyer.save
+          @flyer.upload_flyer(params['flyer'])
+          @flyer.save
          end
 
-        event = Event.new
-        event.event_name = params['event_name']
-        event.event_address = params['event_address']
-        event.venue = params['venue']
-        event.flyer_id = flyer.id
-        event.event_start_time = params['event_start_time']
-        event.event_notes1_label = params['event_notes1_label']
-        event.event_notes1 = params['event_notes1']
-        event.event_notes1_label = params['event_notes2_label']
-        event.event_notes1 = params['event_notes2']
-        event.event_notes1_label = params['event_notes3_label']
-        event.event_notes1 = params['event_notes3']
-        event.save!
+        @event.event_name = params['event_name']
+        @event.event_address = params['event_address']
+        @event.venue = params['venue']
+        @event.flyer_id = flyer.id
+        @event.event_start_time = params['event_start_time']
+        @event.event_notes1_label = params['event_notes1_label']
+        @event.event_notes1 = params['event_notes1']
+        @event.event_notes1_label = params['event_notes2_label']
+        @event.event_notes1 = params['event_notes2']
+        @event.event_notes1_label = params['event_notes3_label']
+        @event.event_notes1 = params['event_notes3']
+        @event.save!
         
         flash[:notice] = "Event Updated"
       end
 
       render :create
     end  
+
+    def change_upcoming_event
+      @upcoming_event = UpcomingEvent.find_by_sql("select * from upcoming_events where id = #{params['id']}").first
+      @upcoming_event.event_id = params['event_id']
+      @upcoming_event.save!
+      @upcoming_event.reload
+      render :text => @upcoming_event.event.event_name
+    end
   end
 end
--- app/models/upcoming_event.rb
+++ app/models/upcoming_event.rb
@@ -4,4 +4,20 @@ class UpcomingEvent < ActiveRecord::Base
 	
 	ASIAN = 'a'
     MIXED = 'm'
+	MAX_EVENTS = 3
+
+    def UpcomingEvent.synchronize_upcoming_events
+      @upcoming_asian_events = UpcomingEvent.find_by_sql("select * from upcoming_events ue where event_type = 'a' order by event_order") 
+      if @upcoming_asian_events.size < UpcomingEvent::MAX_EVENTS
+        for i in @upcoming_asian_events.size..UpcomingEvent::MAX_EVENTS-1
+          UpcomingEvent.create(:event_order => i+1, :event_type => UpcomingEvent::ASIAN)
+        end
+      end
+      @upcoming_mixed_events = UpcomingEvent.find_by_sql("select * from upcoming_events where event_type = 'm' order by event_order") 
+      if @upcoming_mixed_events.size < UpcomingEvent::MAX_EVENTS
+        for i in @upcoming_mixed_events.size..UpcomingEvent::MAX_EVENTS-1
+          UpcomingEvent.create(:event_order => i+1, :event_type => UpcomingEvent::MIXED)
+        end
+      end
+    end
 end
